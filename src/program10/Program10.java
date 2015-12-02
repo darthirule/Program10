2606246616980872a0a826888101ec64bdeac59d
Sean Buchas <seanbuchas@yahoo.com>
2015-12-01 20:37:48 -0500

    Added assignment details.

--- src/program10/Program10.java
+++ src/program10/Program10.java
@@ -1,7 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * (Use the GregorianCalendar class) Java API has the GregorianCalendar class in the java.util package, 
+ * which you can use to obtain the year, month and day of a date. 
+ * The no-arg constructor constructs an instance for the current date, and the methods get(GregorianCalendar.YEAR), 
+ * get(GregorianCalendar.MONTH), and get(GregorianCalendar.DAY_OF_MONTH) return the year, month, and day. Write a program to perform two tasks:
+
+ *  Display the current year month and day
+ *  The GregorianCalendar class has the setTimeInMillis(long) which can be used to set a specified elapsed time since January 1, 1970. Set the value to 1234567898765L and display the year, month, and day.
+
+ * Submit both source code and compiled code ( .java and .class files )
  */
 package program10;
 
